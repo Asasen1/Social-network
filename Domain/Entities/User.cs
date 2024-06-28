93b66bfaa39263b52bb296ead102ebe02cdc5245
Artemiy <artemiy1astanin@gmail.com>
2024-06-28 23:35:20 +0300

    Update domain models

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -7,26 +7,49 @@ namespace Domain.Entities;
 public class User : Entity
 {
     public FullName FullName { get; private set; }
-    public DateTimeOffset BirthDate { get; private set; }
-    private static readonly List<User> Friends = [];
+    public DateOnly BirthDate { get; private set; }
+    public string Description { get; private set; }
+    public DateTimeOffset CreatedDate { get; private set; }
+    public IReadOnlyList<User> Friends => _friends;
+    private readonly List<User> _friends = [];
+    public IReadOnlyList<Post> Posts => _posts;
+    private readonly List<Post> _posts = [];
+    public IReadOnlyList<Photo> Photos => _photos;
+    private readonly List<Photo> _photos = [];
     
-    private User(Guid id, FullName fullName, DateTimeOffset birthDate) : base(id)
+    private User(
+        Guid id, 
+        FullName fullName, 
+        DateOnly birthDate, 
+        string description, 
+        DateTimeOffset createdDate)
+        : base(id)
     {
         FullName = fullName;
         BirthDate = birthDate;
+        Description = description;
+        CreatedDate = createdDate;
     }
-    
-    public static Result<User> Create(Guid id, string firstName, string secondName, DateTimeOffset birthDate)
+
+    public static Result<User> Create(
+        Guid id, 
+        string firstName,
+        string secondName,
+        DateOnly birthDate, 
+        string description, 
+        DateTimeOffset createdDate)
     {
         return new User(
             id,
             FullName.Create(firstName, secondName).Value,
-            birthDate);
+            birthDate,
+            description,
+            DateTimeOffset.UtcNow);
     }
 
-    public static Result<List<User>> AddFriend(User friend)
+    public Result<List<User>> AddFriend(User friend)
     {
-        Friends.Add(friend);
-        return Friends;
+        _friends.Add(friend);
+        return _friends;
     }
 }
