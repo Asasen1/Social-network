fa2f0d29cb76c7a3d5943d97f11630eb150f6d7e
Artemiy <artemiy1astanin@gmail.com>
2024-07-02 23:02:00 +0300

    Add WriteDbContext

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 using API.Validation;
 using Application;
 using Infrastructure;
+using Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +15,15 @@ builder.Services.AddFluentValidationAutoValidation(configuration =>
 });
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
+
+
 var app = builder.Build();
+var scope = app.Services.CreateScope();
+
+var dbContext = scope.ServiceProvider.GetRequiredService<WriteDbContext>();
+
+await dbContext.Database.MigrateAsync();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ Infrastructure/DbContexts/WriteDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.DbContexts;
+
+public class WriteDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    public WriteDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("SocialNetwork"));
+        optionsBuilder.UseSnakeCaseNamingConvention();
+        optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(WriteDbContext).Assembly,
+            type => type.FullName?.Contains("Configurations.Write") ?? false);
+    }
+
+    private DbSet<User> Users => Set<User>();
+}
